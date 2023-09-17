3ca6d271a63747df6b135a930ffccc4d1c3d9e1f
eskilmo <eoestboe@gmail.com>
2023-09-17 17:12:02 +0200

    handler

--- src/main/java/NesteRundeKalkulator/NesteRundeHandler.java
+++ src/main/java/NesteRundeKalkulator/NesteRundeHandler.java
@@ -1,13 +1,36 @@
 package NesteRundeKalkulator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class NesteRundeHandler {
     
-    public void saveToDatabase() {
+    public void saveSession(Session session, String filename) throws FileNotFoundException {
+        try(PrintWriter writer=new PrintWriter(new File(getFilePath(filename)))) {
+            writer.println("YOYOYO");
+        }
+
 
     }
 
-    public void getDatabase() {
+    public void getSession(String filename) throws FileNotFoundException {
+            try(Scanner reader=new Scanner(new File(getFilePath(filename)))) {
 
+            }
+    }
+
+    public static String getFilePath(String filename) {
+        System.out.println(NesteRundeHandler.class.getResource("").getFile() + filename + ".txt");
+        return NesteRundeHandler.class.getResource("").getFile() + filename + ".txt";
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        NesteRundeHandler handler = new NesteRundeHandler();
+        Session session = new Session("samf");
+
+        handler.saveSession(session, "savedSessions");
     }
 
 }
