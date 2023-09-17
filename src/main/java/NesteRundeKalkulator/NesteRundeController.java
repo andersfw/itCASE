b088e9d616f7bfdf28d25d5d21fd94b8427adec8
Håkon <99hakon@hotmail.no>
2023-09-17 14:47:57 +0200

    controller

--- src/main/java/NesteRundeKalkulator/NesteRundeController.java
+++ src/main/java/NesteRundeKalkulator/NesteRundeController.java
@@ -1,9 +1,34 @@
 package NesteRundeKalkulator;
 
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class NesteRundeController {
+
+    Session session;
     
+    @FXML Button newSessionButton;
+    @FXML TextField sessionName;
+
+
 
     public void handleNewSession() {
-        
+        String name = sessionName.getText();
+        this.session = new Session(name);
+
+        newSessionButton.setVisible(false);
+        sessionName.setVisible(false);
     }
 }
