fb8265f2ee36ed04874ea7b90a6ca6e0116963a1
Håkon <99hakon@hotmail.no>
2023-09-17 13:35:47 +0200

    session

--- src/main/java/NesteRundeKalkulator/Session.java
+++ src/main/java/NesteRundeKalkulator/Session.java
@@ -6,10 +6,36 @@ import java.util.ArrayList;
 public class Session {
     private List<User> users = new ArrayList<User>();
     private String name;
+
     public Session(List<User> users, String name) {
         this.users = users;
         this.name = name;
     }
+
+    public List<User> getUsers() {
+        return users;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        checkName(name);
+        this.name = name;
+    }
+
+    private void checkName(String name) {
+        if (!users.isEmpty()) {
+            for (User user : users) {
+                if (user.getName().equals(name)) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+    }
+
+    
 
     
 }
