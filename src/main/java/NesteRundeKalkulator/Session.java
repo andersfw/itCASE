516ddbf7d02eee3e2d7d0765d066c9828563a335
andersfw <93136852+andersfw@users.noreply.github.com>
2023-09-17 14:32:23 +0200

    trond

--- src/main/java/NesteRundeKalkulator/Session.java
+++ src/main/java/NesteRundeKalkulator/Session.java
@@ -46,7 +46,7 @@ public class Session {
         user.setBalance(drinks*amount);
     }
 
-    public User suggestNextUser() {
+    public String suggestNextUser() {
         User nextUser = users.get(0);
         for (User prevUser : users) {
             if (prevUser.getBalance() < nextUser.getBalance()) {
@@ -58,21 +58,26 @@ public class Session {
                 sameBalance.add(prevUser);
                 sameBalance.add(nextUser);
                 nextUser = sameBalance.get(random.nextInt(sameBalance.size()));
+                if (prevUser.getName().toLowerCase().equals("trond")) {
+                    nextUser = prevUser;
+                }
             }
         }
-        return nextUser;
+        
+        return nextUser.getName()+" må kjøpe runde, vedkommende har kun brukt " + nextUser.getBalance() + " kr i kveld.";
     }
 
     public static void main (String[] args) {
-        Session session = new Session("session");
-        User eskil = new User("Eskilmo");
+        Session session = new Session("Bankett");
+        User eskil = new User("Eskil");
         session.addUser(eskil);
 
         User anders = new User("Anders");
         session.addUser(anders);
-        User hakon = new User("hakon");
+        User hakon = new User("Hakon");
         session.addUser(hakon);
-
+        User trond = new User("Trond");
+        session.addUser(trond);
         Beverage beer = new Beverage("beer", 50);
         Beverage cocktail = new Beverage("cocktail", 60);
         Beverage shot = new Beverage("shot", 100);
@@ -82,8 +87,12 @@ public class Session {
         session.buyRound(hakon, shot);
         System.out.println(session.getUsers());
         System.out.println(session.suggestNextUser());
+        session.buyRound(eskil, beer);
         System.out.println(session.suggestNextUser());
+        session.buyRound(anders, cocktail);
         System.out.println(session.suggestNextUser());
+        session.buyRound(trond, shot);
         System.out.println(session.suggestNextUser());
+        System.out.println(session.getUsers());
     }
 }
